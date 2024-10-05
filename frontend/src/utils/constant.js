44ad73e3cbe70f85d438fe436ba6995a3a55c52b
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-10-05 11:40:22 +0530

    Changes in frontend constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,7 +4,7 @@
 // export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
 
 
-export const USER_API_END_POINT = "https://job-seekr-kfx2.vercel.app/api/v1/user";
-export const JOB_API_END_POINT = "https://job-seekr-kfx2.vercel.app/api/v1/job";
-export const COMPANY_API_END_POINT = "https://job-seekr-kfx2.vercel.app/api/v1/company";
-export const APPLICATION_API_END_POINT = "https://job-seekr-kfx2.vercel.app/api/v1/application";
+export const USER_API_END_POINT = "https://job-seekr-backend.vercel.app/api/v1/user";
+export const JOB_API_END_POINT = "https://job-seekr-backend.vercel.app/api/v1/job";
+export const COMPANY_API_END_POINT = "https://job-seekr-backend.vercel.app/api/v1/company";
+export const APPLICATION_API_END_POINT = "https://job-seekr-backend.vercel.app/api/v1/application";
